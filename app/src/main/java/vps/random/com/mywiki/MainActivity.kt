565b5ef97b25bc63d36cdf82ad6b435fb93d644c
vpsidar <vpmsidar@gmail.com>
2021-06-18 13:54:52 +0530

    Initial commit

---
+++ app/src/main/java/vps/random/com/mywiki/MainActivity.kt
@@ -0,0 +1,11 @@
+package vps.random.com.mywiki
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
